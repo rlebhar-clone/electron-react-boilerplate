2efdee78f3bfffa49d9c5f486e54c98c986b93ee
rlebhar-clone <robin.lebhar@getclone.io>
2024-08-19 18:41:25 +0200

    wip

--- src/renderer/pages/Home/Home.tsx
+++ src/renderer/pages/Home/Home.tsx
@@ -1,14 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { InputLLM } from '../../components/features/InputLLM';
 
 export function Home() {
   const [value, setValue] = useState<string>('');
-  const handleGlobalShortcut = (e: { data: { shortcut: string } }) => {
-    console.log('global-shortcut', e);
-  };
-  useEffect(() => {
-    window.electron.ipcRenderer.on('global-shortcut', handleGlobalShortcut);
-  }, []);
 
   return (
     <div id="container" className="w-[500px] p-1">
